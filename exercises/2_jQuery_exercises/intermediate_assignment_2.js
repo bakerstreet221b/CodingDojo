0b3a09a76d4580612f2423f98577fde46275c8ea
Theres Scherrer <theresscherrer@gmail.com>
2013-10-15 14:25:24 -0700

    Removed excess code

--- exercises/2_jQuery_exercises/intermediate_assignment_2.js
+++ exercises/2_jQuery_exercises/intermediate_assignment_2.js
@@ -1,21 +1,31 @@
 $(document).ready(function(){
-	$(function() {
-	    $( "#sortable_pics" ).sortable();
-	    $( "#sortable_pics" ).disableSelection();
-	});
+
+    $( "#sortable_pics" ).sortable();
+    $( "#sortable_pics" ).disableSelection();
+
 	$('#ninja1').click(function(){
-		$(this).html('<img src="pics/cat1.png">');		
+		$(this).fadeOut(function(){
+			$(this).fadeIn('slow').html('<img src="pics/cat1.png">');
+		});
 	});
 	$('#ninja2').click(function(){
-		$(this).html('<img src="pics/cat2.png">');		
-	});
+		$(this).fadeOut(function(){
+			$(this).fadeIn('slow').html('<img src="pics/cat2.png">');
+		});
+	});	
 	$('#ninja3').click(function(){
-		$(this).html('<img src="pics/cat3.png">');		
+		$(this).fadeOut(function(){
+			$(this).fadeIn('slow').html('<img src="pics/cat3.png">');
+		});
 	});
 	$('#ninja4').click(function(){
-		$(this).html('<img src="pics/cat4.png">');		
+		$(this).fadeOut(function(){
+			$(this).fadeIn('slow').html('<img src="pics/cat4.png">');
+		});
 	});
 	$('#ninja5').click(function(){
-		$(this).html('<img src="pics/cat5.png">');		
+		$(this).fadeOut(function(){
+			$(this).fadeIn('slow').html('<img src="pics/cat5.png">');
+		});		
 	});
 });
