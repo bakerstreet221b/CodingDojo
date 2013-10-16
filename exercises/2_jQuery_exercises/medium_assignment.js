128553e3bbb215c3fd23ab19b87c94eb84aa0059
Theres Scherrer <theresscherrer@gmail.com>
2013-10-16 13:02:34 -0700

    Rearranged and deleted files

--- exercises/2_jQuery_exercises/medium_assignment.js
+++ exercises/2_jQuery_exercises/medium_assignment.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
 
-$('img').click(function(){
-		$(this).fadeTo(100, 0.0);
+	$('img').click(function(){
+		$(this).hide();
 	});
 
-$('#restore').click(function(){
-	$('img').fadeTo(100, 1.0);
-})
+	$('#restore').click(function(){
+		$('img').show();
+	})
 
 });
--- exercises/2_jQuery_exercises/medium_assignment_fade.js
+++ exercises/2_jQuery_exercises/medium_assignment_fade.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
 
-$('img').click(function(){
+	$('img').click(function(){
 		$(this).fadeTo(250, 0.0);
 	});
 
-$('#restore').click(function(){
-	$('img').fadeTo(250, 1.0);
-})
+	$('#restore').click(function(){
+		$('img').fadeTo(250, 1.0);
+	});
 
 });
