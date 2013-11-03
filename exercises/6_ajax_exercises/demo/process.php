44c012bd880d57edd5b6d61a7958e7098f0c6cc2
Theres Scherrer <theresscherrer@gmail.com>
2013-11-03 01:50:40 -0700

    Added query for name and date inputs

--- exercises/6_ajax_exercises/demo/process.php
+++ exercises/6_ajax_exercises/demo/process.php
@@ -1,9 +1,69 @@
 <?php
 	require("connection.php");
 
-	$query = "SELECT * FROM leads WHERE first_name LIKE '{$_POST['name']}%' OR last_name LIKE '{$_POST['name']}%'";
-	$users = fetch_all($query);
-	
+	function given($param) {
+		return strlen(@$_POST[$param]) > 0;
+	}
+
+	$name = mysql_real_escape_string(@$_POST['name']);
+
+	if (given('from_date') && given('to_date') && given('name')) {
+
+		$from_date = $_POST['from_date'];
+		$to_date = $_POST['to_date'];
+
+		$new_from_date = new DateTime($from_date);
+		$new_to_date = new DateTime($to_date);
+		$start = $new_from_date->format('Y-m-d');
+		$end = $new_to_date->format('Y-m-d');
+
+		
+		$query = "SELECT * FROM leads 
+			WHERE (first_name LIKE '{$name}%' OR last_name LIKE '{$name}%') 
+			AND registered_datetime BETWEEN '".$start."' AND '".$end."'ORDER by leads_id DESC";
+	}
+	else if (given('from_date') && !given('to_date') && given('name')) {
+
+		$from_date = $_POST['from_date'];
+
+		$new_from_date = new DateTime($from_date);
+		$start = $new_from_date->format('Y-m-d');
+		
+		$query = "SELECT * FROM leads 
+			WHERE (first_name LIKE '{$name}%' OR last_name LIKE '{$name}%') 
+			AND registered_datetime > '".$start."' ORDER by leads_id DESC";
+	}
+	else if (given('from_date') && !given('to_date') && !given('name')) {
+
+		$from_date = $_POST['from_date'];
+
+		$new_from_date = new DateTime($from_date);
+		$start = $new_from_date->format('Y-m-d');
+		
+		$query = "SELECT * FROM leads 
+			WHERE registered_datetime > '".$start."' ORDER by leads_id DESC";
+	}
+	else if (given('from_date') && given('to_date') && !given('name')) {
+		$from_date = $_POST['from_date'];
+		$to_date = $_POST['to_date'];
+
+		$new_from_date = new DateTime($from_date);
+		$new_to_date = new DateTime($to_date);
+		$start = $new_from_date->format('Y-m-d');
+		$end = $new_to_date->format('Y-m-d');
+		
+		$query = "SELECT * FROM leads 
+			WHERE registered_datetime BETWEEN '".$start."' 
+			AND '".$end."'ORDER by leads_id DESC";
+	}
+	else {
+      	$query = "SELECT * FROM leads 
+		WHERE first_name LIKE '{$name}%' 
+		OR last_name LIKE '{$name}%'";
+	}
+
+	$users = fetch_all($query);		
+
 	$html = "<table class='table table-hover'>
 				<thead>
 					<tr>
@@ -17,11 +77,13 @@
 				<tbody>";
 	foreach ($users as $user) 
 	{
+		$date = new DateTime($user['registered_datetime']);
+		$date = $date->format('Y-m-d');
 		$html .= "<tr>
 					<td>".$user['leads_id']."</td>
 					<td>".$user['first_name']."</td>
 					<td>".$user['last_name']."</td>
-					<td>".$user['registered_datetime']."</td>
+					<td>".$date."</td>
 					<td>".$user['email']."</td>
 				</tr>";	
 	}
