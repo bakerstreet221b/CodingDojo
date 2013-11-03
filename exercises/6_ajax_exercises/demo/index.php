3170d50abc06c89d820dfac090669b697516f6e1
Theres Scherrer <theresscherrer@gmail.com>
2013-11-03 01:53:27 -0700

    Update table when date input changes

--- exercises/6_ajax_exercises/demo/index.php
+++ exercises/6_ajax_exercises/demo/index.php
@@ -5,15 +5,21 @@
 <html>
 <head>
 	<title>Ajax - lead example</title>
-	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../Basic_1/bootstrap.css">
 	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/ui-lightness/jquery-ui.min.css" type="text/css" media="all" />
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 	 <script type="text/javascript">
 	 	$(document).ready(function(){
-	 		$('.datepicker').datepicker();	
+
+	 		$('.datepicker').datepicker({
+
+			    onSelect: function() {
+			    	$('#test_form').submit();			        
+			     }
+			});	
 	 		 		
-	 		$('#search_text').keyup(function(){
+	 		$('input').keyup(function(){
 	 			$('#test_form').submit();
 	 		});
 
