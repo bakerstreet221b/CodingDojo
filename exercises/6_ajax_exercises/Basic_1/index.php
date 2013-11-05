0aa9325f73b536122aa748c02909daa59e1c8d08
Theres Scherrer <theresscherrer@gmail.com>
2013-11-05 11:02:52 -0800

    Added functions to edit and delete notes

--- exercises/6_ajax_exercises/Basic_1/index.php
+++ exercises/6_ajax_exercises/Basic_1/index.php
@@ -1,7 +1,8 @@
 <?php
 	require('connection.php');
-
 	
+
+	$posts = fetch_all("SELECT * FROM notes");
 
 	
 ?>
@@ -15,49 +16,166 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 	<style type="text/css">
-	#box {
-		float: overflow;
-	}
-	.postit {
-		border: 1px solid black;
-		background-color: yellow;
-		height: 150px;
-		width:  150px;
+	div {
+		display: inline-block;
 		margin: 10px;
 		text-align: center;
+	}
+	h4 {
+		background-color: #FFDE00;
+		/*border: 1px solid #FFDE00;*/
+		margin-bottom: 0;
+		padding: 10px;
+	}
+	textarea {
+		margin-bottom: 10px;
+	}
+	.box {
+		padding: 10px;
+		/*border: 1px 	solid black;*/
+		height: 350px;
+		width: 204px;
+		vertical-align: top;
+	}
+
+	.del_form .btn {
 		float: left;
 	}
-	.float_right {
+	.edit_form .btn {
 		float: right;
+	}
+	.postit {
+		border: 1px solid #FFDE00;
+		background-color: #FFED73;
+		font-family: courier;
+		height: 200px;
+		width:  180px;
+		margin: 0 -20px 10px -20px;
+		padding: 10px;
+		text-align: center;
 	}
 	</style>
 	<script type="text/javascript">
 		$(document).ready(function(){
-			
-			$('#post_its').submit(function(){
+
+			$('.del_form').on("submit",function(){
+				// alert('qwerrty');
+				var form = $(this);
 				$.post(
 					$(this).attr('action'), $(this).serialize(), function(data){
-						$('#box').html(data.html);						
+						// alert("ajax");
+						$(form.parent()).remove();
 					}, "json");
 				return false;
 			});
-			$('#post_its').submit();
+
+
+			function divClicked() {
+			    var divHtml = $(this).text();
+			    var editableText = $("<textarea rows='10' cols='26' name='description'></textarea>");
+			    editableText.val(divHtml);
+			    $(this).empty();
+			    $(this).append(editableText);
+			    editableText.focus();
+			}
+
+			$(".postit").click(divClicked);
+			
+			
+			$('#post_its').on ("submit", function(){				
+				$.post(
+					$(this).attr('action'), $(this).serialize(), function(param){
+						$('#box').append(param.html);
+						
+						$('.del_form').on("submit",function(){				
+							var form = $(this);
+							$.post(
+								$(this).attr('action'), $(this).serialize(), function(data){									
+									$(form.parent()).remove();
+								}, "json");
+							return false;
+						});
+
+						// $(".postit").click(divClicked);
+
+						// $('.edit_form').on("submit",function(){
+						// 	var form = $(this);				// alert('qwerrty');
+						// 	$.post(
+						// 		$(this).attr('action'), $(this).serialize(), function(data){
+						// 			// alert('ajax');
+						// 			$(form.parent().html(data.html);
+						// 		}, "json");
+						// 	return false;
+						// });
+					}, "json");
+				return false;
+			});
+
+			
+
+			$('.edit_form').on("submit",function(){
+				var form = $(this);				// alert('qwerrty');
+				$.post(
+					$(this).attr('action'), $(this).serialize(), function(data){
+						// alert('ajax');						
+						$(form.parent()).html(data.html);
+					}, "json");
+				return false;
+			});
+
+			
+			
+
+			
+			    
+			
+			
 		});
 	</script>
 </head>
 <body>
 	<div class='container'>
-		<h1>My Posts:</h1>	
-		<div id='box'></div>
-		<form class='navbar' action='process.php' method='post' id='post_its'>
+		<h1>My Posts:</h1>
+		<!-- <div id='test'></div> -->
+		<div id='box'>
+		<?php
+			foreach ($posts as $post) {
+				echo "<div class='box'>					
+					<h4 class='title'>".$post['title']."</h4>
+					<form action='process.php' method='post' id='edit_form' class='edit_form' >
+					<div id='".$post['id']."' class='postit'>".$post['description']."</div>
+					
+						<input type='hidden' name='note_id' value='".$post['id']."'>
+						<input type='hidden' name='action' value='edit_note'>
+						<input type='submit' class='btn btn-default btn-sm' value='edit'>
+				 	</form>
+					<form action='process.php' method='post' id='del_form' class='del_form' >
+						<input type='hidden' name='note_id' value='".$post['id']."'>
+						<input type='hidden' name='action' value='delete_note'>
+						<input type='submit' class='btn btn-default btn-sm' value='delete'>
+					</form>
+					
+
+					</div>";
+			};
+			
+			
+		?>
+		<!-- <img src='delete.png' id='".$post['id']."' class='delete_img'> -->
+		</div>
+		
+		
+		<form class='navbar' action='process.php' method='post' id='post_its'>			
+
 			<div class='form-group'>
-			<textarea class='form-control' type='text' placeholder='Add a note:' name='note'></textarea>
+			<input class='form-control' type='text' placeholder='Insert note title here: ' name='title'>
 			</div>
-			<input class='btn btn-primary' type='submit' value='Post It!'>
+			<input type='hidden' name='action' value='post_note'>
+			<input class='btn btn-primary' type='submit' id='title_submit' value='Post It!'>
 		</form>
-		<form action='process.php' method='post'><input class='btn btn-danger' type='submit' value='Delete All' name='delete'></form>
+		<form action='process.php' method='post'><input type='hidden' name='action' value='delete_all'><input class='btn btn-danger' type='submit' value='Delete All' name='delete'></form>
 	<hr>
-	<footer>by Thereza Scherrer, 2013</footer>
+	<footer>Made with love by Thereza, 2013</footer>
 	</div>
 </body>
 </html>
--- exercises/6_ajax_exercises/Basic_1/process.php
+++ exercises/6_ajax_exercises/Basic_1/process.php
@@ -1,26 +1,76 @@
 <?php
-	require('connection.php');
+require('connection.php');
 
-	if (isset($_POST['note']) AND $_POST['note'] != '') {
-		$query = "INSERT INTO notes (notes, created_at) VALUES ('".$_POST['note']."', NOW())";
-		mysql_query($query);
-	};
+if(isset($_POST['action']) && $_POST['action'] == "post_note")
+{
+	postAction();
+}
+elseif(isset($_POST['action']) && $_POST['action'] == "delete_note")
+{
+	deleteAction();	
+}
+elseif(isset($_POST['action']) && $_POST['action'] == "delete_all")
+{
+	deleteAllAction();
+}
+elseif(isset($_POST['action']) && $_POST['action'] == "edit_note")
+{
+	postUpdateAction();
+}
 
+function deleteAction()
+{
+	mysql_query("DELETE FROM notes WHERE id = {$_POST['note_id']}");
+
+	echo json_encode("deleted");
+}
+
+function deleteAllAction()
+{
 	if(isset($_POST['delete'])){
-		$query = "truncate table notes";
+			$query = "truncate table notes";
+			mysql_query($query);
+			header('location: index.php');
+		};
+}
+
+function postAction()
+{
+	if (isset($_POST['title']) AND $_POST['title'] != '') 
+	{
+		$query = "INSERT INTO notes (title, created_at) VALUES ('".$_POST['title']."', NOW())";
 		mysql_query($query);
-		header('location: index.php');
-	};
-	
-	
-	$posts = fetch_all("SELECT * FROM notes");
-	// var_dump($posts);
-	$html = NULL;
-	foreach ($posts as $post) {
-		$html .= "<div class='postit'>".$post['notes']."</div>";
-	};
+	}
+		
+	$posts = fetch_all("SELECT id FROM notes");
+	$end = end($posts);
+	$last_post = fetch_all("SELECT * FROM notes WHERE id = ".$end['id']);
+	$html =	"<div class='box'>
+		<h4 class='title'>".$last_post[0]['title']."</h4>
+		<form action='process.php' method='post' class='edit_form' >
+			<div id='".$last_post[0]['id']."' class='postit'>".$last_post[0]['description']."</div>		
+			<input type='hidden' name='note_id' value='".$last_post[0]['id']."'>
+			<input type='hidden' name='action' value='edit_note'>
+			<input type='submit' class='btn btn-default btn-sm' value='Edit'>
+		</form>
+		<form action='process.php' method='post' class='del_form' >
+			<input type='hidden' name='note_id' value='".$last_post[0]['id']."'>
+			<input type='hidden' name='action' value='delete_note'>
+			<input type='submit' class='btn btn-default btn-sm' value='Delete'>
+		</form></div>";
 
 	$data['html'] = $html;
 	echo json_encode($data);
+}
 
+function postUpdateAction()
+{
+	if (isset($_POST['description']) AND $_POST['description'] != '') 
+	{
+		$query = "UPDATE notes SET description='{$_POST['description']}', updated_at=NOW() WHERE id=".$_POST['note_id'];
+		mysql_query($query);
+
+		echo json_encode("updated");	
+	};	
+}
 ?>
