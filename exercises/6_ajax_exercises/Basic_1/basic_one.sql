f03bd1f9a55b32cc7b810580fa3c878e50350376
Theres Scherrer <theresscherrer@gmail.com>
2013-11-10 16:15:12 -0800

    Changed mysql table columns

--- exercises/6_ajax_exercises/Basic_1/basic_one.sql
+++ exercises/6_ajax_exercises/Basic_1/basic_one.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 02, 2013 at 12:48 AM
+-- Generation Time: Nov 06, 2013 at 04:47 AM
 -- Server version: 5.5.29
 -- PHP Version: 5.4.10
 
@@ -22,20 +22,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `notes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `notes` text,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
   `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `notes`
 --
 
-INSERT INTO `notes` (`id`, `notes`, `created_at`) VALUES
-(1, 'Hello World!', '2013-11-01 16:47:53'),
-(2, 'Lorem', '2013-11-01 16:48:02'),
-(3, 'Ipsum', '2013-11-01 16:48:10'),
-(4, 'Dolor', '2013-11-01 16:48:20'),
-(5, 'Sit', '2013-11-01 16:48:24'),
-(6, 'Amet', '2013-11-01 16:48:31'),
-(7, '!', '2013-11-01 16:48:38');
+INSERT INTO `notes` (`id`, `title`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'Lorem', 'You can see how I lived before I met you. Also Zoidberg. Fry! Quit doing the right thing, you jerk!', '2013-11-05 19:33:14', '2013-11-05 19:45:55'),
+(2, 'Ipsum', 'You can see how I lived before I met you. Also Zoidberg. Fry! Quit doing the right thing, you jerk! ', '2013-11-05 19:33:17', '2013-11-05 19:46:14'),
+(3, 'Dolor', 'Then throw her in the laundry room, which will hereafter be referred to as "the brig". ', '2013-11-05 19:33:20', '2013-11-05 19:42:08'),
+(4, 'Sit', 'You can see how I lived before I met you. Also Zoidberg. Fry! Quit doing the right thing, you jerk!', '2013-11-05 19:33:21', '2013-11-05 19:34:51'),
+(5, 'Amet', 'Why are you so buggy?\r\nAnd why are you working now?', '2013-11-05 19:33:24', '2013-11-05 19:40:14');
