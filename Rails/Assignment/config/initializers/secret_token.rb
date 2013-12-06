af49d2020833261f1e6d8ee5b44578be99d1560b
Theres Scherrer <theresscherrer@gmail.com>
2013-12-05 22:28:52 -0800

    post and user pages have different layouts

---
+++ Rails/Assignment/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Assignment::Application.config.secret_key_base = 'dfd2c308862798d20301e530617bc4e1b47574296944a0a3ee35994ba35ea4cfbf4db5bb4dcf814874b0e0a035a4f3155fa2b1af0572bc40324db01f5b060b91'
