12e435f97524aac89ca658f248e43fd507638fb0
Theres Scherrer <theresscherrer@gmail.com>
2013-12-06 23:44:31 -0800

    Validates email and max length of name

--- Rails/Assignment/app/models/user.rb
+++ Rails/Assignment/app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 	validates :first_name, :last_name, :email_address, :presence => true
-	validates :email_address, :uniqueness => true
+	validates :first_name, :last_name, :length => { :maximum => 50 }
+
+	email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+	validates :email_address, :format => { :with => email_regex }, :uniqueness => true
+
 
 	has_secure_password
 end
