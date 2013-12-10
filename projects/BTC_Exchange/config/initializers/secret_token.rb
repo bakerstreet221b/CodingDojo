b5c633b1e72ccc4f389df8397309fdd805d02a01
Theres Scherrer <theresscherrer@gmail.com>
2013-12-09 22:48:58 -0800

    Started new project.

---
+++ projects/BTC_Exchange/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BTCExchange::Application.config.secret_key_base = '1beee29e3a6f3bf96dbdc213d8f27744823ee1be377d8bae32e3c7c280877b18adb637bf3ca4e460e228a378bce135f7a633c4ff4859b024f041f60573a4845b'
---
+++ projects/BTC_Exchange/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BTCExchange::Application.config.session_store :cookie_store, key: '_BTC_Exchange_session'
