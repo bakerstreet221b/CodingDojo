8bbc7b82f8626e3aa3dcb46da159a93bf7a007ce
Theres Scherrer <theresscherrer@gmail.com>
2013-12-10 20:19:32 -0800

    Added Sessions to project.

---
+++ projects/BTC_Exchange/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+
+
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email]).try(:authenticate, params[:session][:password])
+
+  	if user == false || user.nil?
+  		flash.now[:error] = "Invalid email/password combination."
+  		render :new
+  	else
+  		sign_in user
+  		redirect_to user
+  	end
+  end
+
+  def destroy
+  	sign_out
+  	redirect_to signin_path
+  end
+end
