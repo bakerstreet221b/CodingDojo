a8a14202ced50f08e3c86a150f9348a223b07174
Theres Scherrer <theresscherrer@gmail.com>
2013-12-10 18:57:32 -0800

    Configured get routes to Sign in and out

--- projects/BTC_Exchange/config/routes.rb
+++ projects/BTC_Exchange/config/routes.rb
@@ -3,9 +3,9 @@ BTCExchange::Application.routes.draw do
   get "order_sells/index"
     resources :users
     resources :sessions, :only => [:new, :create, :destroy]
-    match "/signup", :to => 'users#new'
-    match "/signin", :to => 'sessions#new'
-    match "/signout", :to => 'sessions#destroy'
+    get "/signup", :to => 'users#new'
+    get "/signin", :to => 'sessions#new'
+    delete "/signout", :to => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
