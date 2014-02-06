1db1a784888fe34cea983378f3bba5ac66b2a9b3
Theres Scherrer <theresscherrer@gmail.com>
2014-02-06 11:59:26 -0800

    Read and write comments directly underneath posts on index page

--- projects/Mixi/app/controllers/posts_controller.rb
+++ projects/Mixi/app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   include CommentsHelper
 
   def index
-  	@posts = Post.all
+  	@posts = Post.all.order(created_at: :desc)
 
     @post = Post.new
   end
--- projects/Mixi/app/helpers/comments_helper.rb
+++ projects/Mixi/app/helpers/comments_helper.rb
@@ -6,4 +6,8 @@ module CommentsHelper
   def display_comments(post_id)
     @comments = Comment.where(:post_id => post_id)
   end
+
+  def display_last_two(post_id)
+    @display_last_two = Comment.where(:post_id => post_id).last(2)
+  end
 end
